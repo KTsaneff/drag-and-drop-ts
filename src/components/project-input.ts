15b98470910be211070d6319b1e6afbdd6c0c54c
KTsaneff <78788769+KTsaneff@users.noreply.github.com>
2025-10-24 18:09:22 +0300

    refactor(architecture): separated logic into namespaces and modular folders

---
+++ src/components/project-input.ts
@@ -0,0 +1,90 @@
+/// <reference path="base-component.ts" />
+/// <reference path="../decorators/autobind.ts" />
+/// <reference path="../util/validation.ts" />
+/// <reference path="../state/project-state.ts" />
+
+namespace App {
+
+    export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
+        titleInputElement: HTMLInputElement;
+        descriptionInputElement: HTMLInputElement;
+        peopleInputElement: HTMLInputElement;
+
+        constructor() {
+            super('project-input', 'app', true, 'user-input');
+
+            this.titleInputElement = this.element.querySelector(
+                '#title'
+            ) as HTMLInputElement;
+            this.descriptionInputElement = this.element.querySelector(
+                '#description'
+            ) as HTMLInputElement;
+            this.peopleInputElement = this.element.querySelector(
+                '#people'
+            ) as HTMLInputElement;
+
+            this.configure();
+        }
+
+        configure() {
+            this.element.addEventListener('submit', this.submitHandler);
+        }
+
+        renderContent() {}
+
+        private gatherUserInput(): [string, string, number] | void {
+            const enteredTitle = this.titleInputElement.value.trim();
+            const enteredDescription = this.descriptionInputElement.value.trim();
+            const enteredPeople = this.peopleInputElement.value;
+
+            const titleValidatable: Validatable = {
+                value: enteredTitle,
+                required: true,
+                minLenght: 3,
+                maxLenght: 20
+            };
+
+            const descriptionValidatable: Validatable = {
+                value: enteredDescription,
+                maxLenght: 30
+            };
+
+            const peopleValidatable: Validatable = {
+                value: +enteredPeople,
+                required: true,
+                min: 1,
+                max: 5
+            }
+
+            if (
+                !validate(titleValidatable) ||
+                !validate(descriptionValidatable) ||
+                !validate(peopleValidatable)
+            ) {
+                alert('Invalid input, please try again!');
+                return;
+            } else {
+                return [enteredTitle, enteredDescription, +enteredPeople]
+            }
+        }
+
+        private clearInputs () {
+            this.titleInputElement.value = '';
+            this.descriptionInputElement.value = '';
+            this.peopleInputElement.value = '';
+        }
+
+        @autobind
+        private submitHandler(event: Event) {
+            event.preventDefault();
+            const userInput = this.gatherUserInput();
+
+            if(Array.isArray(userInput)) {
+                const [title, desc, people] = userInput;
+                projectState.addProject(...userInput);
+                console.log(title, desc, people);
+                this.clearInputs();
+            }
+        }
+    }
+}
